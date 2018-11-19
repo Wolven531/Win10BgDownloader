056dedb2f2c95e0922f56aea016b33f12deb8771
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-19 11:46:23 -0700

    Update script with more variables (better reuse / readability)

--- bgDownloader.cmd
+++ bgDownloader.cmd
@@ -2,14 +2,19 @@
 
 @ECHO OFF
 
+SET output_format=jpg
+SET temp_dir_name=temp_bg_img
+
 SET img_dir=%userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
-SET temp_img_dir=%img_dir%\temp_bg_img
+SET temp_img_dir=%img_dir%\%temp_dir_name%
+SET temp_img_pattern=%temp_img_dir%\*.%output_format%
 
 CD "%img_dir%"
 MKDIR "%temp_img_dir%"
-COPY /n /-y "%img_dir%" "%temp_img_dir%\*.jpg"
+COPY /n /-y "%img_dir%" "%temp_img_pattern%"
 
 ECHO Opening explorer to new directory with images ("%temp_img_dir%")...
+
 explorer.exe %temp_img_dir%
 
 @ECHO ON
