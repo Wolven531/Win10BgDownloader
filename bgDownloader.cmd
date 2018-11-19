87cb8b1dd1f0aea1701b5546a3213a59d0f3e5bb
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-19 11:08:25 -0700

    Script now creates temp dir in img dir and copies files as JPG images

--- bgDownloader.cmd
+++ bgDownloader.cmd
@@ -3,8 +3,13 @@
 @ECHO OFF
 
 SET img_dir=%userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+SET temp_img_dir=%img_dir%\temp_bg_img
 
-ECHO Opening explorer to directory with images ("%img_dir%")...
-explorer.exe %img_dir%
+CD "%img_dir%"
+MKDIR "%temp_img_dir%"
+COPY /n /-y "%img_dir%" "%temp_img_dir%\*.jpg"
+
+ECHO Opening explorer to new directory with images ("%temp_img_dir%")...
+explorer.exe %temp_img_dir%
 
 @ECHO ON
