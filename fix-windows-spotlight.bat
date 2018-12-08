dda86a9b93e50ec884993373d82f07d6275792f5
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-07 18:20:17 -0700

    Added script to fix windows spotlight issues (use with caution)

---
+++ fix-windows-spotlight.bat
@@ -0,0 +1,11 @@
+::
+:: Copied from:
+:: https://www.tenforums.com/tutorials/82156-reset-re-register-windows-spotlight-windows-10-a.html
+::
+
+:: Reset Windows Spotlight
+DEL /F /S /Q /A "%USERPROFILE%/AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+DEL /F /S /Q /A "%USERPROFILE%/AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\Settings"
+
+:: Re-register Windows Spotlight
+PowerShell -ExecutionPolicy Unrestricted -Command "& {$manifest = (Get-AppxPackage *ContentDeliveryManager*).InstallLocation + '\AppxManifest.xml' ; Add-AppxPackage -DisableDevelopmentMode -Register $manifest}"
