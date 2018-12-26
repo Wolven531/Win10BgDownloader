c4abf793e4d79c2c61c6f19e35e5098dceb73f8c
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-26 18:39:26 -0500

    Fix image relocation to portrait and landscape dirs

--- bg-downloader.ps1
+++ bg-downloader.ps1
@@ -6,57 +6,60 @@ Add-Type -AssemblyName System.Drawing
 $output_format = 'jpg'
 $temp_dir_name = 'temp_bg_img'
 
-$img_dir = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
-$temp_img_dir = "$img_dir\$temp_dir_name"
-$landscape_dir = "$temp_img_dir\landscape"
-$portrait_dir = "$temp_img_dir\portrait"
+$img_data_dir = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+$temp_dir = "$img_data_dir\$temp_dir_name"
+$landscape_dir = "$temp_dir\landscape"
+$portrait_dir = "$temp_dir\portrait"
 
-If ((Test-Path -Path "$temp_img_dir") -Eq $False)
+If ((Test-Path -Path "$temp_dir") -Eq $False)
 {
-	New-Item -ItemType Directory -Path "$img_dir" -Name "$temp_dir_name"
+	New-Item -ItemType Directory -Path "$img_data_dir" -Name "$temp_dir_name"
 }
 If ((Test-Path -Path "$landscape_dir") -Eq $False)
 {
 	Write-Host "Could not find LANDSCAPE directory at '$landscape_dir', creating..."
-	New-Item -ItemType Directory -Path "$temp_img_dir" -Name 'landscape'
+	New-Item -ItemType Directory -Path "$temp_dir" -Name 'landscape'
 }
 If ((Test-Path -Path "$portrait_dir") -Eq $False)
 {
 	Write-Host "Could not find PORTRAIT directory at '$portrait_dir', creating..."
-	New-Item -ItemType Directory -Path "$temp_img_dir" -Name 'portrait'
+	New-Item -ItemType Directory -Path "$temp_dir" -Name 'portrait'
 }
 
-$images = Get-ChildItem -Path "$img_dir" -File
+$images = Get-ChildItem -Path "$img_data_dir" -File
 
 ForEach($image in $images)
 {
-	$currentLocation = $image.FullName
-	$newImageName = $image.Name
+	$imageDataFile = $image.FullName
+	$filenameWithExt = $image.Name
 
-	If (-Not ($image.Extension -Eq ".$output_format"))
+	If ($image.Extension -Ne ".$output_format")
 	{
-		$newImageName = $newImageName + ".$output_format"
+		$filenameWithExt = $filenameWithExt + ".$output_format"
 	}
 
-	Write-Host "Copying file '$newImageName'..."
-	Copy-Item -LiteralPath "$currentLocation" -Destination "$temp_img_dir\$newImageName"
-	$newLocation = "$temp_img_dir\$newImageName"
-	$jpgImage = [System.Drawing.Image]::FromFile($newLocation)
+	$tempLocation = "$temp_dir\$filenameWithExt"
+
+	Copy-Item -LiteralPath "$imageDataFile" -Destination "$tempLocation"
+	$jpg = [System.Drawing.Image]::FromFile($tempLocation)
 
 	# NOTE: this is landscape
-	If ($jpgImage.Height -eq 1080 -and $jpgImage.Width -eq 1920)
+	If ($jpg.Height -eq 1080 -and $jpg.Width -eq 1920)
 	{
-		Write-Host "Moving file '$newImageName' to landscape..."
-		Move-Item -LiteralPath $newLocation -Destination $landscape_dir
+		Write-Host "[MOVE-TO-LAND] tempLocation='$tempLocation' landscape_dir='$landscape_dir'"
+		Move-Item -LiteralPath "$imageDataFile" -Destination "$landscape_dir\$filenameWithExt"
 	}
 	# NOTE: this is portrait
-	ElseIf ($jpgImage.Height -eq 1920 -and $jpgImage.Width -eq 1080)
+	ElseIf ($jpg.Height -eq 1920 -and $jpg.Width -eq 1080)
 	{
-		Write-Host "Moving file '$newImageName' to portrait..."
-		Move-Item -LiteralPath $newLocation -Destination $portrait_dir
+		Write-Host "[MOVE-TO-PORT] tempLocation='$tempLocation' portrait_dir='$portrait_dir'"
+		Move-Item -LiteralPath "$imageDataFile" -Destination "$portrait_dir\$filenameWithExt"
 	}
 }
 
-Write-Host "Opening explorer to new directory with images ('$temp_img_dir')..."
+# TODO: ajw - Dec 26, 2018; make sure the following command works
+Remove-Item * -Include "*.$output_format" -Force
 
-Invoke-Item -Path "$temp_img_dir"
+Write-Host "Opening explorer to new directory with images ('$temp_dir')..."
+
+Invoke-Item -Path "$temp_dir"
